8ae71a28e45fb64d1b21941642483182f1a89a87
PLQin <773561801@qq.com>
2017-01-07 18:20:31 +0800

    2017.1.7
    2017.1.7

---
+++ reactApp/App.js
@@ -0,0 +1,46 @@
+/*
+
+var HellowDemo = React.createClass({
+    TextCenter : {
+        textAlign : "center"
+    },
+    DataArr : [
+        <h1>菜鸟教程</h1>,
+        <h2>学的不仅是技术，更是梦想！</h2>,
+    ],
+    render  : function(){
+        return (
+            <div style={this.TextCenter}>
+                <h1>Hello World!!!</h1>
+                <h3>开始学习ReactJS!！！！</h3>
+                <div>{this.DataArr}</div>
+            </div>
+        );
+    }
+}) ;
+React.render(
+    <HellowDemo/>,
+    document.getElementById('app')
+);
+*/
+
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
